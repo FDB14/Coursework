63ffcab606dd4c5419b162b44ea5ad6d57402f30
FDB <freddie1debruyn@gmail.com>
2023-03-26 02:22:58 +0100

    Add new background colours as well as new parallax effects 🚀

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './assets/header.jsx'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Header></Header>
+  
+      <Parallax pages={2}>
+        <ParallaxLayer offset={0}>
+          <Header></Header>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={5}>
+          <Header></Header>
+        </ParallaxLayer>
+
+      </Parallax>
+         
   )
 }
 
--- vite-project/src/assets/header.jsx
+++ vite-project/src/assets/header.jsx
@@ -1,39 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
-import football from './football.jpg'
+import football from './football.png'
 
 function Header() {
 
-    const [count, setCount] = useState(0);
-    const backgroundImage = './football.jpg'
-
-
     return (
-        <div className='text-xl overflow-hidden'>
-            
-            <Parallax pages={2}>
-                
-                <ParallaxLayer offset={0.2} className='p-0 m-0'>
-                    <h2 className='text-5xl font-extrabold font-serif text-center'>Welcome To My Website</h2>
-                </ParallaxLayer>
-                
-                <ParallaxLayer offset={0.8} speed={0.5}>
-                    <h2 className='text-5xl font-extrabold font-serif text-center'>Goodbye</h2>
-                </ParallaxLayer>
-                
-                <ParallaxLayer 
-                offset={1}  
-                speed={0.05} 
-                factor={1}
-                style={{
-                    backgroundImage : `url(${football})`,
-                    backgroundSize : 'cover'                
-                }}
-                >
-                </ParallaxLayer>
-
-            </Parallax>
-        
+        <div className='p-5 text-white font-extrabold font-sans bg-cyan-900/30 outline outline-1 outline-slate-200/20'>
+            Hello World
         </div>
         
     )
