3105828a5fc91087ed94cdd16b4f4b9cf4ae76f2
FDB <freddie1debruyn@gmail.com>
2023-04-06 17:56:19 +0100

    Use api-football to get player's data 😇

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'c3fea4af30msh2c3b781cdc7cd06p18ae75jsne303f5bef812',
+        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+    }
+};
+
+function fetchPlayerData(page){
+        //fetch player data (page is a changing variable with a recursive function)
+        fetch(`https://api-football-v1.p.rapidapi.com/v3/players?league=39&season=2022&page=${page}`, options)
+        .then(response => response.json())      
+        .then(response => {
+            if(page <= response.paging.total){
+                
+                console.log(response)
+
+                setTimeout(() => fetchPlayerData(page + 1), 5000)
+        }})
+        .catch(err => console.error(err))
+    }
+
+//call fetch function (argument corresponds to the first page)
+fetchPlayerData(1)
