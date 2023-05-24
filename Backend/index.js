ca5377ff6bdcd7e211b9cb88e5bbf4f8fb2bc7d5
FDB <freddie1debruyn@gmail.com>
2023-05-25 00:07:37 +0100

    Added objects for players data. DATABASE NEXT 💾

--- Backend/index.js
+++ Backend/index.js
@@ -11,13 +11,40 @@ function fetchPlayerData(page){
         fetch(`https://api-football-v1.p.rapidapi.com/v3/players?league=39&season=2022&page=${page}`, options)
         .then(response => response.json())
         .then(response => {
-            if(page <= response.paging.total){
-                let answer = response.response[1]
-                console.log(answer)
+            function playerData(){
+                    let answer = response.response[1]
+                    let currentPage = response.paging.current
+                    let lastPage = response.paging.total    
+                    return [answer, currentPage, lastPage]
+                }
+            return playerData()
+        })
+        .then(response => {
+            const object = {
+                id : response[0].player.id, 
+                playerName : response[0].player.name,
+                playerLast : response[0].player.lastname,
+                age : response[0].player.age,
+                nationality : response[0].player.nationality,
+                height : response[0].player.height
+            }
+            currentPage = response[1]
+            lastPage = response[2]
+            return [object, currentPage, lastPage]
+        })
+        .then(response => {
+            console.log(response[0])
+            currentPage = response[1]
+            lastPage = response[2]
+            return [currentPage, lastPage]
+        })
+        .then(response => {
+            if(response[0] <= response[1]){
                 setTimeout(() => fetchPlayerData(page + 1), 5000)
-        }})
+            }
+        })
         .catch(err => console.error(err))
 }
 
 //call fetch function (argument corresponds to the starting page)
-fetchPlayerData(1)
+let temp = fetchPlayerData(1)
