412210c94ac80a73b31c668c277520176626dae5
FDB <freddie1debruyn@gmail.com>
2023-10-29 13:58:35 +0000

    Added quicksort algorithm to sort players by rating

--- myapp/src/app/Playerselect/page.js
+++ myapp/src/app/Playerselect/page.js
@@ -19,13 +19,29 @@ client.end()
 
 export default async function Playerselect() {
 
+    function quickSort(arr){
+        if(arr.length < 2){
+            return arr
+        }
+        let pivot = arr[arr.length - 1]
+        let right = []
+        let left = []
+        for(let i = 0; i < arr.length -1; i++){
+            if(arr[i].rating >= pivot.rating){
+                left.push(arr[i])
+            }else{
+                right.push(arr[i])
+            }
+        }
+        return[...quickSort(left), pivot, ...quickSort(right)]
+    }
+
     return(
         <div className = "flex min-h-screen flex-col p-12 items-center bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-900 to-gray-600">
             <NavBar1>
             </NavBar1>
-            <DefenderTable Defenders={Defenders}>
+            <DefenderTable Defenders={quickSort(Defenders)}>
             </DefenderTable>
         </div>
     )
 }
-
---
+++ myapp/src/app/Playerselect/test.js
@@ -0,0 +1,4 @@
+let Defenders = [{'rating':9.11, 'shush' : 'hello'}, {'rating' : 9.45}, {'rating' : 3498}]
+
+console.log(Defenders['rating'])
+
